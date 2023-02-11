b399b91a733cc7a135d8b9ef816becb445d806f9
Tri Nguyen <tnguy@mit.edu>
2023-02-11 15:51:56 -0500

    remove lina's old code

--- lina_old_codes/vel_disp_code/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
-
-extensions = [Extension("*", ["*.pyx"])]
-
-setup(
-    ext_modules = cythonize(extensions)
-)
